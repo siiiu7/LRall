2433c1878db9e68a2d03172e42b15abed2ec8328
siiiu7 <124711298+siiiu7@users.noreply.github.com>
2023-05-25 17:14:37 +0400

    Update LR-6 24 вариант 2 часть.py

--- LR-6 24 вариант 2 часть.py
+++ LR-6 24 вариант 2 часть.py
@@ -5,7 +5,7 @@
 Сформировать (вывести) все возможные варианты меню полдника (1 фрукт) для ребенка на неделю
 '''
 # Ограничения на калорийность
-# Программа выводит все возможные комбинации из 4 фруктов с их калорийностью 
+# Программа выводит все возможные комбинации из 3 фруктов с их калорийностью 
 # и находит комбинацию с самым большим количеством калорий.
 
 fruits = {'ф1': 57, 'ф2': 89, 'ф3': 47, 'ф4': 40, 'ф5': 34, 'ф6': 52, 'ф7': 67, 'ф8': 85}
@@ -19,15 +19,21 @@ def get_combinations(fruits, size, combination, start):
         new_combination = combination + [fruit]
         yield from get_combinations(fruits, size-1, new_combination, index+1)
 
-# Получаем все возможные комбинации из 4 фруктов
-combs = list(get_combinations(fruits, 4, [], 0))
+# Получаем все возможные комбинации из 3 фруктов
+combs = list(get_combinations(fruits, 3, [], 0))
 
-# Создаем список кортежей с комбинациями и их калорийностью
+# Создаем список с комбинациями и их калорийностью
 calories_list = [(comb, sum([fruits[fruit] for fruit in comb])) for comb in combs]
 
 # Сортируем список по убыванию калорийности
 sorted_calories_list = sorted(calories_list, reverse=True, key=lambda x: x[1])  
 
+# Выводим все комбинации и их калорийность
+print("Все возможные комбинации и их калорийность:")
+for comb, calories in sorted_calories_list:
+    print(f"{', '.join(comb)} - {calories} калорий")
+
+# Находим самую калорийную комбинацию
 max_combination, max_calories = sorted_calories_list[0] 
 
-print(f"Самая калорийная комбинация из 4 фруктов - {max_combination} с общей калорийностью {max_calories}")
+print(f"\nСамая калорийная комбинация из 3 фруктов - {', '.join(max_combination)} с общей калорийностью {max_calories} калорий")
